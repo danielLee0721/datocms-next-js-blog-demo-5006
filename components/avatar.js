54d160d6867fb5140c5bebfd11af849eaf56b672
Daniel Lee <daniel@doctormarketing.co>
2020-12-27 22:05:13 -0500

    new commit

--- components/avatar.js
+++ components/avatar.js
@@ -1,12 +1,13 @@
-export default function Avatar({ name, picture }) {
-  return (
-     <div className="flex items-center">
-        <img
-           src={picture.url}
-           className="w-12 h-12 rounded-full mr-4"
-           alt={name}
-        />
-        <div className="text-xl font-bold">{name}</div>
-     </div>
-  );
+export default function Avatar({ name, picture, jobTitle }) {
+   return (
+      <div className="flex items-center">
+         <img
+            src={picture.url}
+            className="w-12 h-12 rounded-full mr-4"
+            alt={name}
+         />
+         <div className="text-xl font-bold">{name}</div>
+         <div className="text-xl">{jobTitle}</div>
+      </div>
+   );
 }
--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ import { request } from "../lib/datocms";
 import { metaTagsFragment, responsiveImageFragment } from "../lib/fragments";
 
 export async function getStaticProps({ preview }) {
-  const graphqlRequest = {
-    query: `
+   const graphqlRequest = {
+      query: `
       {
         site: _site {
           favicon: faviconMetaTags {
@@ -37,6 +37,7 @@ export async function getStaticProps({ preview }) {
             picture {
               url(imgixParams: {fm: jpg, fit: crop, w: 100, h: 100, sat: -100})
             }
+            jobTitle
           }
         }
       }
@@ -44,53 +45,53 @@ export async function getStaticProps({ preview }) {
       ${metaTagsFragment}
       ${responsiveImageFragment}
     `,
-    preview,
-  };
+      preview,
+   };
 
-  return {
-    props: {
-      subscription: preview
-        ? {
-            ...graphqlRequest,
-            initialData: await request(graphqlRequest),
-            token: process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
-          }
-        : {
-            enabled: false,
-            initialData: await request(graphqlRequest),
-          },
-    },
-  };
+   return {
+      props: {
+         subscription: preview
+            ? {
+                 ...graphqlRequest,
+                 initialData: await request(graphqlRequest),
+                 token: process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
+              }
+            : {
+                 enabled: false,
+                 initialData: await request(graphqlRequest),
+              },
+      },
+   };
 }
 
 export default function Index({ subscription }) {
-  const {
-    data: { allPosts, site, blog },
-  } = useQuerySubscription(subscription);
+   const {
+      data: { allPosts, site, blog },
+   } = useQuerySubscription(subscription);
 
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
-  const metaTags = blog.seo.concat(site.favicon);
+   const heroPost = allPosts[0];
+   const morePosts = allPosts.slice(1);
+   const metaTags = blog.seo.concat(site.favicon);
 
-  return (
-    <>
-      <Layout preview={subscription.preview}>
-        <Head>{renderMetaTags(metaTags)}</Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
-  );
+   return (
+      <>
+         <Layout preview={subscription.preview}>
+            <Head>{renderMetaTags(metaTags)}</Head>
+            <Container>
+               <Intro />
+               {heroPost && (
+                  <HeroPost
+                     title={heroPost.title}
+                     coverImage={heroPost.coverImage}
+                     date={heroPost.date}
+                     author={heroPost.author}
+                     slug={heroPost.slug}
+                     excerpt={heroPost.excerpt}
+                  />
+               )}
+               {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+            </Container>
+         </Layout>
+      </>
+   );
 }
